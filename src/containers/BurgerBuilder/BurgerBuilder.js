927d2c8584c50486751907cd5561525620605c97
michaltruszczynski <michal.truszczynski2@gmail.com>
2018-10-08 14:49:55 +0200

    modal showhide

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,7 +27,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchaising: false
     }
 
     updatePurchaseState (ingredients) {
@@ -81,6 +82,10 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () => {
+        this.setState({ purchaising: true })
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -91,7 +96,7 @@ class BurgerBuilder extends Component {
         // {salad: true, meat: true, ...}
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchaising}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -100,6 +105,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     price={this.state.totalPrice}
                 />
             </Aux>
