2adb94fc0811ea0169fc2c695e7f15a3a6837df7
michaltruszczynski <michal.truszczynski2@gmail.com>
2018-10-15 19:33:46 +0200

    orders section finished

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -6,7 +6,9 @@ import classes from './NavigationItem.css';
 const navigationItem = ( props ) => (
     <li className={classes.NavigationItem}>
         <NavLink
-            to={props.link}>{props.children} </NavLink>
+            to={props.link}
+            exact={props.exact}
+            activeClassName={classes.active}>{props.children} </NavLink>
     </li>
 );
 
